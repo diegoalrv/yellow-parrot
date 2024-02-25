6a880175b75be504d15071ebff974e6256862bcd
diegoalrv <d.alexis.ramirez@gmail.com>
2024-02-25 17:53:32 -0300

    working first chain

--- scripts/test_app_v1.py
+++ scripts/test_app_v1.py
@@ -1,9 +1,6 @@
 import langchain
-import json
-
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+import json
 
 def main():
     print(langchain.__version__)
@@ -20,17 +17,7 @@ def main():
     llm = ChatOpenAI(openai_api_key=openai_api_key)
 
     print(llm.invoke("how can langsmith help with testing?"))
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are world class technical documentation writer."),
-        ("user", "{input}")
-    ])
-
-    output_parser = StrOutputParser()
-
-    chain = prompt | llm | output_parser
-
-    invoke = chain.invoke({"input": "how can langsmith help with testing?"})
-    print(invoke)
+    
     pass
 
 if __name__ == '__main__':
